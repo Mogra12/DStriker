3ddd4a4a96ba8bc95c20da743d25e159d86c3529
ruanv_ <86124740+Mogra12@users.noreply.github.com>
2025-05-21 13:58:14 -0300

    Add files via upload

---
+++ output_config.h
@@ -0,0 +1,24 @@
+#ifndef OUTPUT_CFG
+#define OUTPUT_CFG
+
+#include <iostream>
+
+struct Out_color {
+    std::string red;
+    std::string green;
+    std::string yellow;
+    std::string b_white;
+    std::string reset;
+};
+
+Out_color colors() {
+    Out_color colors;
+    colors.red    = "\033[1;31m";
+    colors.green  = "\033[1;32m";
+    colors.yellow = "\033[1;33m";
+    colors.b_white = "\033[1;37m";
+    colors.reset  = "\033[0m";
+
+    return colors;
+}
+#endif
--- request.h
+++ request.h
@@ -2,6 +2,7 @@
 #define REQUEST_H
 #include <iostream>
 #include <curl/curl.h>
+#include "output_config.h"
 
 struct Config {
     std::string url;
@@ -15,6 +16,7 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* use
 }
 
 void request(Config cfg, std::string line) {
+    Out_color getColors = colors();
     CURL* curl = curl_easy_init();
     if(curl) {
         std::string response;
@@ -27,9 +29,26 @@ void request(Config cfg, std::string line) {
         if(res == CURLE_OK){
             long response_code;
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-            std::cout << "URL: " << url_mod << " - Status Code: " << response_code << std::endl;
+            std::cout << getColors.b_white 
+                      << "[ + ] URL: " 
+                      << getColors.green 
+                      << url_mod 
+                      << getColors.reset 
+                      << " - "
+                      << getColors.b_white
+                      << "Status Code: " 
+                      << (response_code == 200 ? getColors.green : getColors.yellow) 
+                      << response_code
+                      << getColors.reset
+                      << std::endl;
         } else {
-            std::cerr << "Erro CURL: " << curl_easy_strerror(res) << " na URL: " << url_mod << std::endl;
+            std::cerr << getColors.red 
+                      << "[ - ] Erro CURL: " 
+                      << curl_easy_strerror(res) 
+                      << " na URL: " 
+                      << url_mod 
+                      << getColors.reset
+                      << std::endl;
         }
         curl_easy_cleanup(curl);                                       
     }
